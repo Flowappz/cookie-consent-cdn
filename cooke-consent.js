59efa007f5bca40b14f6579f7592db3598000f89
rabbykhairul <rabbykhairul@gmail.com>
2023-12-31 22:24:25 +0600

    get privacy policy link from server by hostname

--- cooke-consent.js
+++ cooke-consent.js
@@ -2,115 +2,132 @@
  * VERSION: 1.0.2
  */
 
-const sheet = new CSSStyleSheet();
-sheet.replaceSync(`
-.flowappz--cookie-container * {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  font-family: inherit;
+function attachCookieConsentStylesheet() {
+  const sheet = new CSSStyleSheet();
+  sheet.replaceSync(`
+  .flowappz--cookie-container * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: inherit;
+  }
+  
+  .flowappz--cookie-container {
+    position: fixed;
+    bottom: 5vh;
+    width: 350px;
+    height: 250px;
+    left: 35px;
+    background-color: white;
+    box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.1);
+    padding: 53px 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  
+  .flowappz--cookie-container.hide {
+    display: none;
+  }
+  
+  .flowappz--cookie-container .cookie-heading {
+    font-weight: bold;
+    color: black;
+    font-size: 26px;
+    line-height: 31.47px;
+  }
+  
+  .flowappz--cookie-container .cookie-description {
+    font-weight: 400;
+    color: black;
+    font-size: 16px;
+    line-height: 19.36px;
+    margin-top: 25px;
+  }
+  
+  .flowappz--cookie-container a {
+    color: black;
+  }
+  
+  .flowappz--cookie-container .button-group {
+    display: flex;
+    gap: 20px;
+  }
+  
+  .flowappz--cookie-container .button {
+    background: black;
+    color: white;
+    padding: 12px 28px;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+  }
+  .flowappz--cookie-container .button:last-child {
+    background-color: white;
+    color: black;
+  }
+  `);
+
+  const sheets = document.adoptedStyleSheets || [];
+  document.adoptedStyleSheets = [...sheets, sheet];
 }
 
-.flowappz--cookie-container {
-  position: fixed;
-  bottom: 5vh;
-  width: 350px;
-  height: 250px;
-  left: 35px;
-  background-color: white;
-  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.1);
-  padding: 53px 50px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
+(async () => {
+  try {
+    attachCookieConsentStylesheet();
+    let privacyPolicyUrl = "#";
 
-.flowappz--cookie-container.hide {
-	display: none;
-}
+    const res = await fetch(`http://localhost:3002/cookie-consent/privacy-policy?hostname=${window.location.hostname}`);
+    if (res.ok) {
+      data = await res.json();
+      privacyPolicyUrl = data.privacyPolicyUrl;
+    }
 
-.flowappz--cookie-container .cookie-heading {
-  font-weight: bold;
-  color: black;
-  font-size: 26px;
-  line-height: 31.47px;
-}
+    const rejectButton = document.createElement("button");
+    rejectButton.setAttribute("class", "button");
+    rejectButton.innerText = "Reject all";
 
-.flowappz--cookie-container .cookie-description {
-  font-weight: 400;
-  color: black;
-  font-size: 16px;
-  line-height: 19.36px;
-  margin-top: 25px;
-}
+    const agreeButton = document.createElement("button");
+    agreeButton.setAttribute("class", "button");
+    agreeButton.innerText = "Accept all";
 
-.flowappz--cookie-container a {
-  color: black;
-}
+    const buttonGroup = document.createElement("div");
+    buttonGroup.setAttribute("class", "button-group");
+    buttonGroup.append(agreeButton, rejectButton);
 
-.flowappz--cookie-container .button-group {
-  display: flex;
-  gap: 20px;
-}
+    const cookieDescription = document.createElement("p");
+    cookieDescription.setAttribute("class", "cookie-description");
 
-.flowappz--cookie-container .button {
-  background: black;
-  color: white;
-  padding: 12px 28px;
-  border: none;
-  font-weight: bold;
-	cursor: pointer;
-}
-.flowappz--cookie-container .button:last-child {
-  background-color: white;
-  color: black;
-}
-`);
+    const descriptionText = document.createTextNode(
+      "We use cookies to improve user experience. Choose what cookies you allow us to use. You can read more about our Cookie Policy in our "
+    );
+    const privacyPolicyLink = document.createElement("a");
+    privacyPolicyLink.innerText = "Privacy Policy";
+    privacyPolicyLink.setAttribute("href", privacyPolicyUrl);
 
-const sheets = document.adoptedStyleSheets || [];
-document.adoptedStyleSheets = [...sheets, sheet];
+    cookieDescription.append(descriptionText, privacyPolicyLink);
 
-const rejectButton = document.createElement("button");
-rejectButton.setAttribute("class", "button");
-rejectButton.innerText = "Reject all";
+    const cookieTitle = document.createElement("h5");
+    cookieTitle.setAttribute("class", "cookie-heading");
+    cookieTitle.innerText = "Cookies";
 
-const agreeButton = document.createElement("button");
-agreeButton.setAttribute("class", "button");
-agreeButton.innerText = "Accept all";
+    const textWrapperDiv = document.createElement("div");
+    textWrapperDiv.append(cookieTitle, cookieDescription);
 
-const buttonGroup = document.createElement("div");
-buttonGroup.setAttribute("class", "button-group");
-buttonGroup.append(agreeButton, rejectButton);
+    const cookieContainer = document.createElement("div");
+    cookieContainer.setAttribute("class", "flowappz--cookie-container");
+    cookieContainer.append(textWrapperDiv, buttonGroup);
 
-const cookieDescription = document.createElement("p");
-cookieDescription.setAttribute("class", "cookie-description");
+    document.querySelector("body").appendChild(cookieContainer);
 
-const descriptionText = document.createTextNode(
-  "We use cookies to improve user experience. Choose what cookies you allow us to use. You can read more about our Cookie Policy in our "
-);
-const privacyPolicyLink = document.createElement("a");
-privacyPolicyLink.innerText = "Privacy Policy";
-privacyPolicyLink.setAttribute("href", "https://google.com");
+    agreeButton.addEventListener("click", () => {
+      cookieContainer.classList.add("hide");
+    });
 
-cookieDescription.append(descriptionText, privacyPolicyLink);
-
-const cookieTitle = document.createElement("h5");
-cookieTitle.setAttribute("class", "cookie-heading");
-cookieTitle.innerText = "Cookies";
-
-const textWrapperDiv = document.createElement("div");
-textWrapperDiv.append(cookieTitle, cookieDescription);
-
-const cookieContainer = document.createElement("div");
-cookieContainer.setAttribute("class", "flowappz--cookie-container");
-cookieContainer.append(textWrapperDiv, buttonGroup);
-
-document.querySelector("body").appendChild(cookieContainer);
-
-agreeButton.addEventListener("click", () => {
-  cookieContainer.classList.add("hide");
-});
-
-rejectButton.addEventListener("click", () => {
-  cookieContainer.classList.add("hide");
-});
+    rejectButton.addEventListener("click", () => {
+      cookieContainer.classList.add("hide");
+    });
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+})();
